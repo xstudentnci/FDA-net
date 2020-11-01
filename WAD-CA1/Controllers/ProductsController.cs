c02d7f710068301da70e7c6f16ac0ac7e40ac46d
Maxi <herreramaxi@gmail.com>
2020-11-01 12:05:41 +0000

    Creating project template

---
+++ WAD-CA1/Controllers/ProductsController.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Text;
+using System.Xml;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+
+namespace WAD_CA1.Controllers
+{
+
+    public class ProductsController : Controller
+    {
+        [HttpGet]
+        public IActionResult DoSomething()
+        {
+            return Ok("hey");
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult GetTable()
+        {
+            var rawHtml = ApplyXSLTransformation();
+
+           return Ok(rawHtml);
+        }
+
+        private static string ApplyXSLTransformation()
+        {
+            using var myXmlFile = System.IO.File.Open("Products.xml", FileMode.Open);
+            XPathDocument myXPathDoc = new XPathDocument(myXmlFile);
+            XslTransform myXslTrans = new XslTransform();
+            using var styleSheetFile = System.IO.File.Open("ProductsTransformation.xsl", FileMode.Open);
+            XmlTextReader stylesheetReader = new XmlTextReader(styleSheetFile);
+            myXslTrans.Load(stylesheetReader);
+            //XmlTextWriter myWriter = new XmlTextWriter("result.html", null);
+
+            StringBuilder resultString = new StringBuilder();
+            using var xmlWriter = new XmlTextWriter(new StringWriter(resultString));
+
+            myXslTrans.Transform(myXPathDoc, null, xmlWriter);
+            return resultString.ToString();
+            //using var reader = new StreamReader(stream);
+            //return reader.ReadToEnd();
+        }
+
+    }
+}
---
+++ WAD-CA1/Controllers/ProductsUpload.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WAD_CA1.Controllers
+{
+    public class ProductsUpload : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("UploadFile")]
+        //[DisableFormValueModelBinding]
+        //[ValidateAntiForgeryToken]
+        public async Task<IActionResult> UploadFile()
+        {
+            if (HttpContext.Request.Form.Files[0] == null)
+            {
+                return Ok("No file selected for upload.");
+            }
+
+            var filePath = Path.GetTempFileName();
+
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                var file = HttpContext.Request.Form.Files[0];
+                await file.CopyToAsync(stream);
+            }
+
+            return Ok("All good");
+        }
+    }
+}
