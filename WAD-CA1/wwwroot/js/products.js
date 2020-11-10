91a8f1f77cc120783d2cdb9e4e0ad3b5b57e55bf
Valeria Etchudez <valeria.etchudez@gmail.com>
2020-11-10 00:13:26 +0000

    INTERNAL: Added search and small styles changes

---
+++ WAD-CA1/wwwroot/js/products.js
@@ -0,0 +1,54 @@
+﻿window.onload = function () {
+    LoadTable();
+};
+
+function LoadTable() {
+    console.log("from document.onload");
+    var oReq = new XMLHttpRequest();
+    oReq.onload = function (e) {
+        console.log("from document.onloadt onload");
+
+        document.getElementById("tableOuput").innerHTML = this.responseText;
+    };
+    oReq.open("get", "Products/GetTable");
+    oReq.send();
+}
+
+function AjaxGet(url) {
+    console.log("from AjaxGet");
+    var oReq = new XMLHttpRequest();
+    oReq.onload = function (e) {
+        console.log("from AjaxGet onload");
+        document.getElementById("result2").innerHTML = 'Result: ' + this.responseText;
+    };
+    oReq.open("get", url);
+    oReq.send();
+}
+
+function Search() {
+    var input, filter, table, tr, td, i, txtValue;
+    input = document.getElementById("searchField");
+    filter = input.value.toUpperCase();
+    table = document.getElementById("tableOuput");
+    var rows = table.getElementsByTagName("td");
+    for (i = 0; i < rows.length; i++) {
+        var cells = rows[i].parentNode.childNodes;
+        var foundInRow = false;
+        for (j = 0; j < cells.length; j++) {
+            var cellValue = cells[j].textContent || row.innerText;
+            debugger;
+            if (cellValue) {
+                txtValue = cellValue;
+                if (txtValue.toUpperCase().indexOf(filter) > -1) {
+                    foundInRow = true;
+                }
+            }
+        }
+        if (foundInRow) {
+            rows[i].style.display = "";
+        } else {
+            rows[i].style.display = "none";
+        }
+    }
+    
+}
